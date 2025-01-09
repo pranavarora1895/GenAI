59816a7a44644f7532f2f13c7434506cd3e5a4fd
Pranav Arora <pranav.arora@anglersolutions.ca>
2025-01-09 13:11:14 -0330

    Add files via upload

---
+++ image_cap.py
@@ -0,0 +1,21 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+img_path = "puppy.jpg"
+
+image = Image.open(img_path).convert('RGB')
+
+text  = "the image of"
+
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+outputs = model.generate(**inputs, max_length=50)
+
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+
+print(caption)
